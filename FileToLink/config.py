5bcb88fc950c1fe65ab91d83f838295ad2bb3137
fpband <82159763+fpband@users.noreply.github.com>
2022-11-14 11:40:51 +0330

    Update config.py

--- FileToLink/config.py
+++ FileToLink/config.py
@@ -18,7 +18,7 @@ class Config:
     elif Bot_Channel == "":
         Bot_Channel = None
 
-    Link_Root = f"https://{App_Name}.herokuapp.com/"
+    Link_Root = f"https://{App_Name}.osc-fr1.scalingo.io/"
     Download_Folder = "Files"
     Dev_Channel = "FarshidBand"
     Bot_UserName = None  # The bot will set it after starting
