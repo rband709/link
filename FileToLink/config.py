9199355b15c23b5a81fab1f20dcd738f21f0dba1
fpband <82159763+fpband@users.noreply.github.com>
2022-11-30 11:36:17 +0330

    Update config.py

--- FileToLink/config.py
+++ FileToLink/config.py
@@ -18,7 +18,7 @@ class Config:
     elif Bot_Channel == "":
         Bot_Channel = None
 
-    Link_Root = f"https://{App_Name}.osc-fr1.scalingo.io/"
+    Link_Root = f"https://{App_Name}.railway.app/"
     Download_Folder = "Files"
     Dev_Channel = "FarshidBand"
     Bot_UserName = None  # The bot will set it after starting
